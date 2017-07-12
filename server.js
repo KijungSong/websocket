b8cbf75e9760ab8c776dfdf7e362975f119f4ff1
Songkijung <skj4522@gmail.com>
2017-07-12 22:04:45 +0900

    hello websocket

---
+++ server.js
@@ -0,0 +1,22 @@
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var port = process.env.PORT || 5000;
+var server = http.createServer();
+server.listen(port);
+
+var wss = new WebSocketServer({
+  server: server
+});
+console.log('websocket server created!');
+
+wss.on('connection', function(ws) {
+  console.log('websocket connection open!');
+
+  ws.on('message', function(message) {
+    console.log(message);
+  });
+
+  ws.on('close', function() {
+    console.log('websocket close');
+  });
+});
